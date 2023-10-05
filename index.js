eb1f48ec4af0bb0c0be74c7475c925c277369d9f
ahmedqadar <abdulnet029@gmail.com>
2023-10-05 20:22:00 +0300

    object lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Ahmed',
+    streetAddress: '30300 Kp'
+}
+
+function updateEmployeeWithKeyAndValue () {
+    const newEmployee = {...employee};
+    newEmployee['name'] = 'Sam';
+    newEmployee['streetAddress'] = '11 Broadway';
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue () {
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+
+function deleteFromEmployeeByKey () {
+   let deleteFromEmployeeByKey = {...destructivelyUpdateEmployeeWithKeyAndValue};
+    delete deleteFromEmployeeByKey['name'];
+    return deleteFromEmployeeByKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey () {
+    delete employee['name'];
+    return employee;
+}
